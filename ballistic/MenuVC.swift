d5c2c9f40cbddc48e4cbbd0acbc733b317223548
jmh3434 <jmh3434@gmail.com>
2018-07-10 22:39:07 -0400

    first commit

--- ballistic/MenuVC.swift
+++ ballistic/MenuVC.swift
@@ -1,9 +1,44 @@
 //
 //  MenuVC.swift
-//  ballistic
+//  Pong2
 //
-//  Created by James Hunt on 7/10/18.
-//  Copyright © 2018 James Hunt. All rights reserved.
+//  Created by Jared Davidson on 1/9/17.
+//  Copyright © 2017 Archetapp. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+enum gameType {
+    case easy
+    case medium
+    case hard
+    case player2
+}
+
+class MenuVC : UIViewController {
+    
+    
+    @IBAction func Player2(_ sender: Any) {
+        moveToGame(game: .player2)
+    }
+    
+    @IBAction func Easy(_ sender: Any) {
+        moveToGame(game: .easy)
+    }
+    @IBAction func Medium(_ sender: Any) {
+        moveToGame(game: .medium)
+        
+    }
+    @IBAction func Hard(_ sender: Any) {
+        moveToGame(game: .hard)
+    }
+    
+    func moveToGame(game : gameType) {
+        let gameVC = self.storyboard?.instantiateViewController(withIdentifier: "gameVC") as! GameViewController
+        
+        currentGameType = game
+        
+        self.navigationController?.pushViewController(gameVC, animated: true)
+    }
+}
