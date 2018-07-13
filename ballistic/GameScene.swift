bdf8a5c851c57d8d6916989135d4b1c6104528a9
jmh3434 <jmh3434@gmail.com>
2018-07-13 15:08:37 -0400

    working rotation

--- ballistic/GameScene.swift
+++ ballistic/GameScene.swift
@@ -10,6 +10,11 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    private let kAnimalNodeName = "movable"
+    //bitmasks
+    let mainCategory:UInt32 = 0x1 << 0 //1
+    let ballCategory:UInt32 = 0x1 << 1        //2
+    //
     
     var ball = SKSpriteNode()
     var enemy = SKSpriteNode()
@@ -19,14 +24,23 @@ class GameScene: SKScene {
     var btmLbl = SKLabelNode()
     
     var score = [Int]()
+    //rotate
+    let rotateRec = UIRotationGestureRecognizer()
+    var theRotation:CGFloat = 0
+    var offset:CGFloat = 0
+    
     
     override func didMove(to view: SKView) {
+        createSprites()
+        //rotate
+        rotateRec.addTarget(self, action: #selector(GameScene.rotatedView(_:)))
+        self.view!.addGestureRecognizer(rotateRec)
         
-        topLbl = self.childNode(withName: "topLabel") as! SKLabelNode
-        btmLbl = self.childNode(withName: "btmLabel") as! SKLabelNode
+        //
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         
-        print(self.view?.bounds.height)
+        print(self.view?.bounds.height as Any)
         
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
         enemy.position.y = (self.frame.height / 2) - 50
@@ -42,53 +56,32 @@ class GameScene: SKScene {
         self.physicsBody = border
         
         startGame()
+        //bitmasks
+        main.physicsBody?.categoryBitMask = mainCategory
+        ball.physicsBody?.collisionBitMask = mainCategory
+        //
     }
-    
-    func startGame() {
-        score = [0,0]
-        topLbl.text = "\(score[1])"
-        btmLbl.text = "\(score[0])"
-        main.zRotation = 2
-        //ball.physicsBody?.applyImpulse(CGVector(dx: 10 , dy: 10))
+    func createSprites() {
+        
         
     }
-    
-    func addScore(playerWhoWon : SKSpriteNode){
-        
-        ball.position = CGPoint(x: 0, y: 0)
-        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        
-       
-        
-        topLbl.text = "\(score[1])"
-        btmLbl.text = "\(score[0])"
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for touch in touches {
-            let location = touch.location(in: self)
-            
-            main.run(SKAction.moveTo(x: location.x, duration: 0.0))
-            main.run(SKAction.moveTo(y: location.y, duration: 0.0))
-            
-            
+    //rotate
+    @objc func rotatedView(_ sender:UIRotationGestureRecognizer){
+        if (sender.state == .began){
+            print("we began")
         }
-        
-    }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            
-            
-            
-            main.run(SKAction.moveTo(x: location.x, duration: 0.0))
-            main.run(SKAction.moveTo(y: location.y, duration: 0.0))
-            
-            
+        if (sender.state == .changed){
+            print("we rotated")
+            theRotation = CGFloat(sender.rotation) + self.offset  
+            theRotation = theRotation * -1
+            main.zRotation = theRotation
+        }
+        if (sender.state == .ended){
+            print("we ended")
+            self.offset = theRotation * -1
         }
     }
-    
+    //
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
@@ -115,7 +108,61 @@ class GameScene: SKScene {
         }
         
     }
-}
+
+    
+    func startGame() {
+        score = [0,0]
+        
+        main.zRotation = 2
+        //ball.physicsBody?.applyImpulse(CGVector(dx: 10 , dy: 10))
+        
+    }
+    
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+       
+        
+        topLbl.text = "\(score[1])"
+        btmLbl.text = "\(score[0])"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+
+            
+            
+        }
+        
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let positionInScene = touch.location(in: self)
+            let touchedNode = self.atPoint(positionInScene)
+            
+            if let name = touchedNode.name
+            {
+                if name == "main"
+                {
+                    main.run(SKAction.moveTo(x: location.x, duration: 0.0))
+                    main.run(SKAction.moveTo(y: location.y, duration: 0.0))
+                }
+                
+            }
+            
+           
+            
+            
+        }
+    }
+    
+    }
 
 
 
